61d4592d12e2bee8187315ec7140d3fae0b33ff8
Kanter <123274048+Kanterer@users.noreply.github.com>
2026-06-19 17:33:12 +0300

    Update ListSequence.hpp

--- include/ListSequence.hpp
+++ include/ListSequence.hpp
@@ -1,203 +1,329 @@
 #ifndef LIST_SEQUENCE_HPP
 #define LIST_SEQUENCE_HPP
+
 #include "LinkedList.hpp"
 #include "Sequence.hpp"
-#include <functional>
 
 template <class T>
-class ListSequence : public Sequence<T> {
+class MutableListSequence;
+
+template <class T>
+class ImmutableListSequence;
+
+template <class T, class Derived>
+class ListSequence : public Sequence<T, Derived> {
 protected:
-    LinkedList<T>* items;
+    LinkedList<T> items;
 
-    void AppendInternal(const T& item) {
-        items->Append(item);
+    Derived* SelfDerived() {
+        return static_cast<Derived*>(this);
     }
 
-    void PrependInternal(const T& item) {
-        items->Prepend(item);
-    }
-
-    void InsertAtInternal(const T& item, int index) {
-        items->InsertAt(item, index);
-    }
-
-    virtual ListSequence<T>* InstanceForChange() {
-        return this;
-    }
-
-    virtual ListSequence<T>* CreateEmpty() const {
-        return new ListSequence<T>();
+    const Derived* SelfDerived() const {
+        return static_cast<const Derived*>(this);
     }
 
 public:
-    ListSequence()
-        : items(new LinkedList<T>()) {}
-
-    ListSequence(const T* sourceItems, int count)
-        : items(new LinkedList<T>(sourceItems, count)) {}
-
-    explicit ListSequence(const LinkedList<T>& list)
-        : items(new LinkedList<T>(list)) {}
-
-    ListSequence(const ListSequence<T>& other)
-        : items(new LinkedList<T>(*other.items)) {}
-
-    ListSequence<T>& operator=(const ListSequence<T>& other) {
-        if (this == &other) {
-            return *this;
-        }
-        delete items;
-        items = new LinkedList<T>(*other.items);
-        return *this;
+    void AppendInternal(T item) {
+        items.Append(item);
     }
 
-    ~ListSequence() override {
-        delete items;
+    void PrependInternal(T item) {
+        items.Prepend(item);
     }
 
-    T GetFirst() const override {
-        return items->GetFirst();
+    void InsertAtInternal(T item, int index) {
+        items.InsertAt(item, index);
     }
 
-    T GetLast() const override {
-        return items->GetLast();
-    }
-
-    T Get(int index) const override {
-        return items->Get(index);
-    }
-
-    int GetLength() const override {
-        return items->GetLength();
-    }
-
-    Sequence<T>* GetSubsequence(int startIndex, int endIndex) const override {
-        const int length = items->GetLength();
-        if (startIndex < 0 || endIndex < 0 || startIndex > endIndex || endIndex >= length) {
-            throw IndexOutOfRange("ListSequence: invalid subsequence indexes");
-        }
-
-        ListSequence<T>* result = CreateEmpty();
-        for (int i = startIndex; i <= endIndex; ++i) {
-            result->AppendInternal(items->Get(i));
-        }
-        return result;
-    }
-
-    Sequence<T>* Append(const T& item) override {
-        ListSequence<T>* result = InstanceForChange();
-        result->AppendInternal(item);
-        return result;
-    }
-
-    Sequence<T>* Prepend(const T& item) override {
-        ListSequence<T>* result = InstanceForChange();
-        result->PrependInternal(item);
-        return result;
-    }
-
-    Sequence<T>* InsertAt(const T& item, int index) override {
-        ListSequence<T>* result = InstanceForChange();
-        result->InsertAtInternal(item, index);
-        return result;
-    }
-
-    Sequence<T>* Concat(const Sequence<T>& sequence) override {
-        ListSequence<T>* result = InstanceForChange();
-        const int sourceLength = sequence.GetLength();
-        for (int i = 0; i < sourceLength; ++i) {
-            result->AppendInternal(sequence.Get(i));
-        }
-        return result;
-    }
-
-    Sequence<T>* Map(const std::function<T(const T&)>& function) const override {
-        ListSequence<T>* result = CreateEmpty();
-        for (int i = 0; i < GetLength(); ++i) {
-            result->AppendInternal(function(Get(i)));
-        }
-        return result;
-    }
-
-    Sequence<T>* Where(const std::function<bool(const T&)>& predicate) const override {
-        ListSequence<T>* result = CreateEmpty();
-        for (int i = 0; i < GetLength(); ++i) {
-            T value = Get(i);
-            if (predicate(value)) {
-                result->AppendInternal(value);
-            }
-        }
-        return result;
-    }
-
-    T Reduce(const std::function<T(const T&, const T&)>& function, const T& startValue) const override {
-        T result = startValue;
-        for (int i = 0; i < GetLength(); ++i) {
-            result = function(result, Get(i));
-        }
-        return result;
-    }
-
-    Sequence<T>* Clone() const override {
-        return new ListSequence<T>(*this);
-    }
-};
-
-template <class T>
-class MutableListSequence : public ListSequence<T> {
 protected:
-    ListSequence<T>* InstanceForChange() override {
-        return this;
+    static int NormalizeSliceStart(int startIndex, int length) {
+        int normalizedStart = startIndex;
+
+        if (normalizedStart < 0) {
+            normalizedStart = length + normalizedStart;
+        }
+
+        return normalizedStart;
     }
 
-    ListSequence<T>* CreateEmpty() const override {
+    static void ValidateSliceRange(int normalizedStart, int count, int length) {
+        if (count < 0) {
+            throw InvalidArgumentException("slice count cannot be negative");
+        }
+
+        if (normalizedStart < 0 || normalizedStart > length) {
+            throw IndexOutOfRangeException("slice start index is outside sequence bounds");
+        }
+
+        if (normalizedStart + count > length) {
+            throw IndexOutOfRangeException("slice removes more elements than sequence contains");
+        }
+    }
+
+public:
+    ListSequence() : items() {}
+
+    ListSequence(T* inputItems, int count) : items(inputItems, count) {}
+
+    explicit ListSequence(const LinkedList<T>& list) : items(list) {}
+
+    ListSequence(const ListSequence<T, Derived>& other) : items(other.items) {}
+
+    T GetFirstImpl() const {
+        return items.GetFirst();
+    }
+
+    T GetLastImpl() const {
+        return items.GetLast();
+    }
+
+    T GetImpl(int index) const {
+        return items.Get(index);
+    }
+
+    MutableListSequence<T>* GetSubsequenceImpl(int startIndex, int endIndex) const {
+        int length = items.GetLength();
+
+        if (startIndex < 0 || endIndex < 0 || startIndex >= length || endIndex >= length) {
+            throw IndexOutOfRangeException("subsequence boundaries are outside ListSequence bounds");
+        }
+
+        if (startIndex > endIndex) {
+            throw InvalidArgumentException("startIndex cannot be greater than endIndex");
+        }
+
+        MutableListSequence<T>* result = new MutableListSequence<T>();
+
+        const typename LinkedList<T>::Node* current = items.head;
+        int currentIndex = 0;
+
+        while (currentIndex < startIndex) {
+            current = current->next;
+            ++currentIndex;
+        }
+
+        while (currentIndex <= endIndex) {
+            result->AppendInternal(current->value);
+            current = current->next;
+            ++currentIndex;
+        }
+
+        return result;
+    }
+
+    int GetLengthImpl() const {
+        return items.GetLength();
+    }
+
+    Derived* AppendImpl(T item) {
+        Derived* target = SelfDerived()->Instance();
+
+        target->AppendInternal(item);
+
+        return target;
+    }
+
+    Derived* PrependImpl(T item) {
+        Derived* target = SelfDerived()->Instance();
+
+        target->PrependInternal(item);
+
+        return target;
+    }
+
+    Derived* InsertAtImpl(T item, int index) {
+        Derived* target = SelfDerived()->Instance();
+
+        target->InsertAtInternal(item, index);
+
+        return target;
+    }
+
+    template <class OtherDerived>
+    Derived* ConcatImpl(const OtherDerived& sequence) {
+        Derived* target = SelfDerived()->Instance();
+        int length = sequence.GetLength();
+
+        for (int i = 0; i < length; ++i) {
+            target->AppendInternal(sequence.Get(i));
+        }
+
+        return target;
+    }
+
+    T& operator[](int index) {
+        return items[index];
+    }
+
+    const T& operator[](int index) const {
+        return items[index];
+    }
+
+    MutableListSequence<T>* EmptyMutableImpl() const {
         return new MutableListSequence<T>();
     }
 
-public:
-    MutableListSequence()
-        : ListSequence<T>() {}
+    MutableListSequence<T>* MapImpl(T (*mapper)(const T&)) const {
+        MutableListSequence<T>* result = new MutableListSequence<T>();
 
-    MutableListSequence(const T* items, int count)
-        : ListSequence<T>(items, count) {}
+        const typename LinkedList<T>::Node* current = items.head;
 
-    explicit MutableListSequence(const LinkedList<T>& list)
-        : ListSequence<T>(list) {}
+        while (current != nullptr) {
+            result->AppendInternal(mapper(current->value));
+            current = current->next;
+        }
 
-    MutableListSequence(const MutableListSequence<T>& other)
-        : ListSequence<T>(other) {}
+        return result;
+    }
 
-    Sequence<T>* Clone() const override {
-        return new MutableListSequence<T>(*this);
+    MutableListSequence<T>* MapIndexedImpl(T (*mapper)(const T&, int)) const {
+        MutableListSequence<T>* result = new MutableListSequence<T>();
+
+        const typename LinkedList<T>::Node* current = items.head;
+        int index = 0;
+
+        while (current != nullptr) {
+            result->AppendInternal(mapper(current->value, index));
+            current = current->next;
+            ++index;
+        }
+
+        return result;
+    }
+
+    MutableListSequence<T>* WhereImpl(bool (*predicate)(const T&)) const {
+        MutableListSequence<T>* result = new MutableListSequence<T>();
+
+        const typename LinkedList<T>::Node* current = items.head;
+
+        while (current != nullptr) {
+            if (predicate(current->value)) {
+                result->AppendInternal(current->value);
+            }
+
+            current = current->next;
+        }
+
+        return result;
+    }
+
+    MutableListSequence<T>* SliceWithoutReplacementImpl(int startIndex, int count) const {
+        int length = items.GetLength();
+        int normalizedStart = NormalizeSliceStart(startIndex, length);
+
+        ValidateSliceRange(normalizedStart, count, length);
+
+        MutableListSequence<T>* result = new MutableListSequence<T>();
+
+        const typename LinkedList<T>::Node* current = items.head;
+        int index = 0;
+
+        while (current != nullptr) {
+            bool shouldRemove = index >= normalizedStart && index < normalizedStart + count;
+
+            if (!shouldRemove) {
+                result->AppendInternal(current->value);
+            }
+
+            current = current->next;
+            ++index;
+        }
+
+        return result;
+    }
+
+    template <class ReplacementDerived>
+    MutableListSequence<T>* SliceImpl(int startIndex, int count, const ReplacementDerived* replacement) const {
+        int length = items.GetLength();
+        int normalizedStart = NormalizeSliceStart(startIndex, length);
+
+        ValidateSliceRange(normalizedStart, count, length);
+
+        MutableListSequence<T>* result = new MutableListSequence<T>();
+
+        const typename LinkedList<T>::Node* current = items.head;
+        int index = 0;
+
+        while (current != nullptr && index < normalizedStart) {
+            result->AppendInternal(current->value);
+            current = current->next;
+            ++index;
+        }
+
+        for (int i = 0; replacement != nullptr && i < replacement->GetLength(); ++i) {
+            result->AppendInternal(replacement->Get(i));
+        }
+
+        int skipped = 0;
+
+        while (current != nullptr && skipped < count) {
+            current = current->next;
+            ++index;
+            ++skipped;
+        }
+
+        while (current != nullptr) {
+            result->AppendInternal(current->value);
+            current = current->next;
+            ++index;
+        }
+
+        return result;
     }
 };
 
 template <class T>
-class ImmutableListSequence : public ListSequence<T> {
-protected:
-    ListSequence<T>* InstanceForChange() override {
-        return new ImmutableListSequence<T>(*this);
-    }
-
-    ListSequence<T>* CreateEmpty() const override {
-        return new ImmutableListSequence<T>();
-    }
-
+class MutableListSequence : public ListSequence<T, MutableListSequence<T>> {
 public:
-    ImmutableListSequence()
-        : ListSequence<T>() {}
+    MutableListSequence() : ListSequence<T, MutableListSequence<T>>() {}
 
-    ImmutableListSequence(const T* items, int count)
-        : ListSequence<T>(items, count) {}
+    MutableListSequence(T* items, int count)
+        : ListSequence<T, MutableListSequence<T>>(items, count) {}
+
+    explicit MutableListSequence(const LinkedList<T>& list)
+        : ListSequence<T, MutableListSequence<T>>(list) {}
+
+    MutableListSequence(const MutableListSequence<T>& other)
+        : ListSequence<T, MutableListSequence<T>>(other) {}
+
+    MutableListSequence<T>* Instance() {
+        return this;
+    }
+
+    MutableListSequence<T>* CloneImpl() const {
+        return new MutableListSequence<T>(*this);
+    }
+
+    bool IsMutableImpl() const {
+        return true;
+    }
+};
+
+template <class T>
+class ImmutableListSequence : public ListSequence<T, ImmutableListSequence<T>> {
+public:
+    ImmutableListSequence() : ListSequence<T, ImmutableListSequence<T>>() {}
+
+    ImmutableListSequence(T* items, int count)
+        : ListSequence<T, ImmutableListSequence<T>>(items, count) {}
 
     explicit ImmutableListSequence(const LinkedList<T>& list)
-        : ListSequence<T>(list) {}
+        : ListSequence<T, ImmutableListSequence<T>>(list) {}
 
     ImmutableListSequence(const ImmutableListSequence<T>& other)
-        : ListSequence<T>(other) {}
+        : ListSequence<T, ImmutableListSequence<T>>(other) {}
 
-    Sequence<T>* Clone() const override {
+    ImmutableListSequence<T>* Instance() {
         return new ImmutableListSequence<T>(*this);
+    }
+
+    ImmutableListSequence<T>* CloneImpl() const {
+        return new ImmutableListSequence<T>(*this);
+    }
+
+    bool IsMutableImpl() const {
+        return false;
     }
 };
 
