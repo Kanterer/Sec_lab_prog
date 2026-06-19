1e28bfc85fdecbe2c71a32c0fa8c131d83331aac
Kanter <123274048+Kanterer@users.noreply.github.com>
2026-06-19 17:35:24 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
-#include "Test.hpp"
+#include "Tests.hpp"
 #include "UI.hpp"
+#include <iostream>
 
 int main() {
-    RunAllTests();
-    RunConsoleInterface();
+    try {
+        RunAllTests();
+    } catch (const std::exception& ex) {
+        std::cerr << "Tests failed: " << ex.what() << std::endl;
+        return 1;
+    }
+
+    RunUI();
+
     return 0;
 }
